cffe460750cb40801cc1cf4f20fc42165a46796e
devfabiopedro <fabio.silvapedro76@gmail.com>
2024-07-31 14:44:16 -0300

    redefine Schema

--- madr/routers/accounts_router.py
+++ madr/routers/accounts_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status
 
-from madr.schemas import AccountSchema
+from madr.schemas import AccountListSchema
 
 router = APIRouter(prefix='/account', tags=['Account'])
 
@@ -8,7 +8,7 @@ router = APIRouter(prefix='/account', tags=['Account'])
 @router.get(
     '/',
     status_code=status.HTTP_200_OK,
-    response_model=AccountSchema,
+    response_model=AccountListSchema,
     name='Read and list all Accounts',
 )
 def read_users(): ...
