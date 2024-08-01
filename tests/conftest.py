5909efc61a58c870bcfdc95b98135eff96fccd41
devfabiopedro <fabio.silvapedro76@gmail.com>
2024-08-01 16:15:09 -0300

    update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy.pool import StaticPool
 
 from madr.app import app
-from madr.models import table_registry
+from madr.database import get_session
+from madr.models import Account, table_registry
 
 
 @pytest.fixture
@@ -26,3 +27,29 @@ def session():
         yield session
 
     table_registry.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture
+def user(session):
+    user = Account(
+        username='AccountTest',
+        email='account.test@email.com',
+        password='accounttest'
+    )
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
