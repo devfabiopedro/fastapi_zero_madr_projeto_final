d82de7c7023e6d6cbb5228fb8d299abf2284aa0e
devfabiopedro <fabio.silvapedro76@gmail.com>
2024-08-02 16:14:22 -0300

    add router auth_token

---
+++ madr/routers/auth_router.py
@@ -0,0 +1,43 @@
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from madr.database import get_session
+from madr.models import Account
+from madr.schemas.token_schema import Token
+from madr.security import create_access_token, verify_password
+
+router = APIRouter(prefix='/auth', tags=['Auth'])
+
+T_OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
+T_Session = Annotated[Session, Depends(get_session)]
+
+
+@router.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: T_OAuth2Form,
+    session: T_Session,
+):
+    user = session.scalar(
+        select(Account).where(Account.email == form_data.username)
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
