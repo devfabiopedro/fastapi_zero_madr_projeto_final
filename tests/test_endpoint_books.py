f69a3adb57bcf43a8de3223b9ab6bdd7cc5fead8
devfabiopedro <fabio.silvapedro76@gmail.com>
2024-08-08 10:20:14 -0300

    new tests for books

--- tests/test_endpoint_books.py
+++ tests/test_endpoint_books.py
@@ -13,27 +13,39 @@ def test_deny_create_book_without_permissions(client):
     assert response.status_code == HTTPStatus.UNAUTHORIZED
 
 
-def test_allow_create_book_with_permissions(client, token):
+def test_allow_create_book_with_permissions(novelist, client, token):
     response = client.post(
         '/books/new',
         headers={'Authorization': f'Bearer {token}'},
         json={
             'year': '2024',
             'title': 'Como ser um melhor praticante de programação!',
-            'novelist_id': 1,
+            'novelist_id': novelist.id,
         },
     )
-    data = response.json()
-    ...
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
         'year': '2024',
-        'title': data['title'],
+        'title': 'como ser um melhor praticante de programação!',
         'novelist_id': 1,
     }
 
 
-def test_get_one_book(client, book):
+def test_try_create_book_without_novelist(client, token):
+    response = client.post(
+        '/books/new',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'year': '2024',
+            'title': 'Como ser um melhor praticante de programação!',
+            'novelist_id': 0,
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail':'Romancista não encontrado'}
+
+
+def test_get_one_book(client, novelist, book):
     response = client.get(f'/books/{book.id}')
     assert response.status_code == HTTPStatus.OK
 
@@ -56,7 +68,7 @@ def test_list_all_books_but_list_empty(client):
     }
 
 
-def test_list_all_books_list_with_book(client, book):
+def test_list_all_books_list_with_book(client, novelist, book):
     response = client.get('books/list')
     assert response.status_code == HTTPStatus.OK
 
@@ -66,7 +78,7 @@ def test_list_all_books_list_with_book(client, book):
     assert response_data['books'][0]['id'] == book.id
 
 
-def test_if_book_exists(client, book, token):
+def test_if_book_exists(client, novelist, book, token):
     response = client.post(
         '/books/new',
         headers={'Authorization': f'Bearer {token}'},
@@ -81,15 +93,25 @@ def test_if_book_exists(client, book, token):
     assert response.json() == {'detail': 'Livro já consta no MADR'}
 
 
-def test_read_books_filter_by_title(client, book):
+def test_read_books_filter_by_title(client, novelist, book):
     response = client.get(
         '/books/list',
-        params={'title': book.title, 'year': book.year},
+        params={'title': book.title},
     )
     assert response.status_code == HTTPStatus.OK
     data = response.json()
-    assert len(data['books']) == 1
+    assert len(data['books']) >= 1
     assert data['books'][0]['title'] == book.title
+
+
+def test_read_books_filter_by_year(client, novelist, book):
+    response = client.get(
+        '/books/list',
+        params={'year': book.year},
+    )
+    assert response.status_code == HTTPStatus.OK
+    data = response.json()
+    assert len(data['books']) >= 1
     assert data['books'][0]['year'] == book.year
 
 
@@ -107,7 +129,7 @@ def test_patch_book_error(client, token):
     assert response.json() == {'detail': 'Livro não consta no MADR'}
 
 
-def test_patch_book(client, book, token):
+def test_patch_book(client, novelist, book, token):
     response = client.patch(
         f'/books/{book.id}',
         json={
@@ -121,7 +143,21 @@ def test_patch_book(client, book, token):
     assert response.json()['title'] == 'um livro qualquer!'
 
 
-def test_delete_book(client, book, token):
+def test_try_update_book_with_inexistent_novelist(client, novelist, book, token):
+    response = client.patch(
+        f'/books/{book.id}',
+        json={
+            'year': '2024',
+            'title': 'Como ser um melhor praticante de programação!',
+            'novelist_id': 4,
+        },
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail':'Romancista não encontrado'}
+
+
+def test_delete_book(client, novelist, book, token):
     response = client.delete(
         f'/books/{book.id}',
         headers={
@@ -134,7 +170,10 @@ def test_delete_book(client, book, token):
 
 def test_delete_book_error(client, token):
     response = client.delete(
-        '/books/2', headers={'Authorization': f'Bearer {token}'}
+        '/books/2',
+        headers={
+            'Authorization': f'Bearer {token}',
+        },
     )
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'Livro não consta no MADR'}
